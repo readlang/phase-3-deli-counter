ee20566e7e6020fb8ff484f86d0bdbf40752ddd6
Read Langworthy <rlangworthy@gmail.com>
2022-02-10 17:04:27 -0800

    commit tree 73443cea7d4cab7eca9baffe8ae74968dc15ba70

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,45 @@
 # Write your code here.
+
+katz_deli = []
+
+def line (array)
+    if array.length == 0
+        puts "The line is currently empty."        
+    else
+        string = "The line is currently:"
+        i = 0
+        while i < array.length
+            string += " #{i+1}. #{array[i]}"
+            i += 1
+        end
+        puts string
+    end
+end
+
+
+def take_a_number (array, string)
+    array.push(string)
+    puts "Welcome, #{string}. You are number #{array.length} in line."
+
+end
+
+def now_serving (array)
+    if array.length == 0
+        puts "There is nobody waiting to be served!"        
+    else
+        puts "Currently serving #{array.first}." 
+        array.shift
+    end
+end
+
+
+
+# take_a_number katz_deli, "Sarah"
+
+# take_a_number katz_deli, "Thomas"
+
+# line katz_deli
+
+# now_serving katz_deli
+
+# now_serving katz_deli
